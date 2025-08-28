a6d32e4f09b47e01ad8c151c3334306a4e37b311
Kaitlin Naughten <kaight@bas.ac.uk>
2025-08-28 16:17:37 +0100

    New temperature correction uncertainty range

--- projects/overshoots.py
+++ projects/overshoots.py
@@ -80,8 +80,8 @@ suites_branched = {'cx209':None, 'cw988':None, 'cw989':None, 'cw990':None, 'cz82
 
 tipping_threshold = -1.9  # If cavity mean temp is warmer than surface freezing point, it's tipped
 temp_correction = 1.5450286133565283 # Precomputed by warming_implied_by_salinity_bias()
-temp_correction_lower = 0.15386275907205732
-temp_correction_upper = 2.004807510160955  # 10-90% bounds precomputed by temp_correction_uncertainty
+temp_correction_lower = 0.5716454706352944
+temp_correction_upper = 2.394992374100425  # 10-90% bounds precomputed by temp_correction_uncertainty
 time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
 
 # End global vars
@@ -3880,7 +3880,7 @@ def temp_correction_uncertainty (base_dir='./', bias_file='bwsalt_bias.nc', slop
 
     sample_file = base_dir+'/time_averaged/piControl_grid-T.nc'
     regions = ['ross', 'filchner_ronne']
-    cutoff = 3
+    cutoff = 3.5
     p0 = 0.05
 
     # Make combined mask
