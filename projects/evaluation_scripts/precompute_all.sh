6363d370974da0f62e45831b8d8f2421b8202f6e
Kaitlin Naughten <kaight@bas.ac.uk>
2025-12-18 15:00:40 +0000

    Add Hovmoller precompute to precompute_all.sh

--- projects/evaluation_scripts/precompute_all.sh
+++ projects/evaluation_scripts/precompute_all.sh
@@ -7,8 +7,9 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem=64GB
 
-# Timeseries - update existing files, or calculate from scratch
+# Timeseries and Hovmollers - update existing files, or calculate from scratch
 python -c "from nemo_python.projects.evaluation import *; update_timeseries_evaluation_NEMO_AIS('./')"
+python -c "from nemo_python.projects.evaluation import *; update_hovmollers_evaluation_NEMO_AIS('./')"
 
 # Time-averaged fields - remove if they exist, then calculate from scratch
 for f in bottom_TS_avg.nc zonal_TS_avg.nc; do
