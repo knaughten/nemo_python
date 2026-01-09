94f496765aa4f97f80636df553dfeb6c3aa5c751
Kaitlin Naughten <kaight@bas.ac.uk>
2026-01-09 14:00:16 +0000

    Update UKESM precompute script

--- projects/evaluation_scripts/precompute_all_ukesm.sh
+++ projects/evaluation_scripts/precompute_all_ukesm.sh
@@ -12,18 +12,17 @@
 if [ ! -d ${SUITE} ]; then
     mkdir ${SUITE}
 fi
+IN_DIR=/gws/ssde/j25b/terrafirma/tarlge/ukesm_monitoring/data/u-${SUITE}
 
 # Timeseries and Hovmollers - update existing files, or calculate from scratch
-python -c "from nemo_python.projects.evaluation import *; update_timeseries_evaluation_UKESM1('"${SUITE}"', in_dir='/gws/ssde/j25b/terrafirma/tarlge/ukesm_monitoring/data/u-"${SUITE}"', out_dir='"${SUITE}"')"
-
-
-python -c "from nemo_python.projects.evaluation import *; update_hovmollers_evaluation_NEMO_AIS('./')"
+python -c "from nemo_python.projects.evaluation import *; update_timeseries_evaluation_UKESM1('"${SUITE}"', in_dir='"${IN_DIR}"', out_dir='"${SUITE}"')"
+python -c "from nemo_python.projects.evaluation import *; update_hovmollers_evaluation_UKESM1('"${SUITE}"', in_dir='"${IN_DIR}"', out_dir='"${SUITE}"')"
 
 # Time-averaged fields - remove if they exist, then calculate from scratch
-for f in bottom_TS_avg.nc zonal_TS_avg.nc; do
-    if [ -f $f ]; then
-	rm $f
-    fi
-done
-python -c "from nemo_python.projects.evaluation import *; precompute_avg(option='bottom_TS', out_file='bottom_TS_avg.nc')"
-python -c "from nemo_python.projects.evaluation import *; precompute_avg(option='zonal_TS', out_file='zonal_TS_avg.nc')"
+#for f in bottom_TS_avg.nc zonal_TS_avg.nc; do
+#    if [ -f $f ]; then
+#	rm $f
+#    fi
+#done
+#python -c "from nemo_python.projects.evaluation import *; precompute_avg(option='bottom_TS', out_file='bottom_TS_avg.nc')"
+#python -c "from nemo_python.projects.evaluation import *; precompute_avg(option='zonal_TS', out_file='zonal_TS_avg.nc')"
