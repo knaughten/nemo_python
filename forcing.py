a6eb3c9364793b4cdf0b431c2af1da7e4d102a35
Kaitlin Naughten <kaight@bas.ac.uk>
2026-01-16 10:57:41 +0000

    Option for raw latitude dimension

--- forcing.py
+++ forcing.py
@@ -461,13 +461,15 @@ def era5_time_mean_forcing(variable, year_start=1979, year_end=2024, freq='daily
         varname = variable
     if processed:
         time_dim = 'time'
+        lat_dim = 'lat'
     else:
         time_dim = 'valid_time'
+        lat_dim = 'latitude'
 
     if freq=='daily' or freq=='hourly':
         if variable in ['wind_speed', 'wind_angle']:
-            era5_u  = xr.open_mfdataset(f'{era5_folder_in}u10_*')['u10'].sortby('lat').sel(lat=lat_slice)
-            era5_v  = xr.open_mfdataset(f'{era5_folder_in}v10_*')['v10'].sortby('lat').sel(lat=lat_slice)
+            era5_u  = xr.open_mfdataset(f'{era5_folder_in}u10_*')['u10'].sortby(lat_dim).sel({lat_dim:lat_slice})
+            era5_v  = xr.open_mfdataset(f'{era5_folder_in}v10_*')['v10'].sortby(lat_dim).sel({lat_dim:lat_slice})
             era5_u  = era5_u.isel({time_dim:((era5_u[time_dim].dt.year <= year_end)*(era5_u[time_dim].dt.year >= year_start)*(era5_u[time_dim].dt.year != 1996))})
             era5_v  = era5_v.isel({time_dim:((era5_v[time_dim].dt.year <= year_end)*(era5_v[time_dim].dt.year >= year_start)*(era5_v[time_dim].dt.year != 1996))})
             if variable == 'wind_speed':
@@ -476,11 +478,11 @@ def era5_time_mean_forcing(variable, year_start=1979, year_end=2024, freq='daily
                 era5_ds = np.arctan2(era5_v, era5_u).rename(variable).to_dataset()
         else:
             try:
-                era5_ds = xr.open_mfdataset(f'{era5_folder_in}{variable}_*.nc')[varname].sortby('lat').sel(lat=lat_slice)
+                era5_ds = xr.open_mfdataset(f'{era5_folder_in}{variable}_*.nc')[varname].sortby(lat_dim).sel({lat_dim:lat_slice})
             except(KeyError):
                 if variable == 'sph2m':
                     varname = 'specific_humidity'
-                era5_ds = xr.open_mfdataset(f'{era5_folder_in}{variable}_*.nc')[varname].sortby('lat').sel(lat=lat_slice)
+                era5_ds = xr.open_mfdataset(f'{era5_folder_in}{variable}_*.nc')[varname].sortby(lat_dim).sel({lat_dim:lat_slice})
             era5_ds = era5_ds.isel({time_dim:((era5_ds[time_dim].dt.year <= year_end)*(era5_ds[time_dim].dt.year >= year_start)*(era5_ds[time_dim].dt.year != 1996))})
 
         time_mean = era5_ds.groupby(time_dim+'.month').mean(dim=time_dim)
@@ -493,8 +495,8 @@ def era5_time_mean_forcing(variable, year_start=1979, year_end=2024, freq='daily
             else:
                 print(year)
                 if variable in ['wind_speed', 'wind_angle']:
-                    era5_u = xr.open_dataset(f'{era5_folder_in}u10_y{year}.nc')['u10'].sortby('lat').sel(lat=lat_slice)
-                    era5_v = xr.open_dataset(f'{era5_folder_in}v10_y{year}.nc')['v10'].sortby('lat').sel(lat=lat_slice)
+                    era5_u = xr.open_dataset(f'{era5_folder_in}u10_y{year}.nc')['u10'].sortby(lat_dim).sel({lat_dim:lat_slice})
+                    era5_v = xr.open_dataset(f'{era5_folder_in}v10_y{year}.nc')['v10'].sortby(lat_dim).sel({lat_dim:lat_slice})
                     era5_u = era5_u.resample({time_dim:'3h'}).mean()
                     era5_v = era5_v.resample({time_dim:'3h'}).mean()           
                     if variable == 'wind_speed':
@@ -503,11 +505,11 @@ def era5_time_mean_forcing(variable, year_start=1979, year_end=2024, freq='daily
                         era5_ds = np.arctan2(era5_v, era5_u).rename(variable).groupby(time_dim+'.month').mean(dim=time_dim).to_dataset()
                 else:
                     try:
-                        era5_ds = xr.open_dataset(f'{era5_folder_in}{variable}_y{year}.nc')[varname].sortby('lat').sel(lat=lat_slice)
+                        era5_ds = xr.open_dataset(f'{era5_folder_in}{variable}_y{year}.nc')[varname].sortby(lat_dim).sel({lat_dim:lat_slice})
                     except(KeyError):
                         if variable == 'sph2m':
                             varname = 'specific_humidity'
-                        era5_ds = xr.open_dataset(f'{era5_folder_in}{variable}_y{year}.nc')[varname].sortby('lat').sel(lat=lat_slice)
+                        era5_ds = xr.open_dataset(f'{era5_folder_in}{variable}_y{year}.nc')[varname].sortby(lat_dim).sel({lat_dim:lat_slice})
                     era5_ds = era5_ds.resample({time_dim:'3h'}).mean().groupby(time_dim+'.month').mean(dim=time_dim).to_dataset()
                
                 era5_ds.to_netcdf(f'{era5_folder_out}ERA5_{variable}_{freq}_monthly_mean_y{year}.nc')
@@ -534,7 +536,7 @@ def era5_clim_for_ukesm (era5_dir='/gws/ssde/j25b/terrafirma/kaight/NEMO_AIS/UKE
             varname = 'avg_sdlwrf'
         else:
             varname = variable
-        era5_time_mean_forcing(variable, year_start=1979, year_end=2014, freq='3-hourly', ere5_folder_in=era5_dir, processed=False, varname=varname)            
+        era5_time_mean_forcing(variable, year_start=1979, year_end=2014, freq='3-hourly', era5_folder_in=era5_dir, processed=False, varname=varname)            
 
 
 # Function calculates the monthly time-mean over specified year range for mean of all CESM2 ensemble members in the specified experiment (for bias correction)
