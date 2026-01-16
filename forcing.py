b839cf22861ea5850cca8915cb32297e52fd3038
Kaitlin Naughten <kaight@bas.ac.uk>
2026-01-16 11:23:45 +0000

    UKESM forcing monthly climatology code

--- forcing.py
+++ forcing.py
@@ -522,9 +522,14 @@ def era5_time_mean_forcing(variable, year_start=1979, year_end=2024, freq='daily
 
 
 # Wrapper for the above to calculate ERA5 climatology for comparison with UKESM, for all variables
-def era5_clim_for_ukesm (era5_dir='/gws/ssde/j25b/terrafirma/kaight/NEMO_AIS/UKESM_forcing/ERA5_hourly/', year_end=2014):
+def era5_clim_for_ukesm (era5_dir='/gws/ssde/j25b/terrafirma/kaight/NEMO_AIS/UKESM_forcing/ERA5_hourly/', year_end=2014, var_name=None):
 
-    for variable in ['t2m', 'sph2m', 'wind_speed', 'wind_angle', 'mtpr', 'msr', 'msl', 'msdwswrf', 'msdwlwrf']:
+    if var_name is None:
+        var_names = [var_name]
+    else:
+        var_names = ['t2m', 'sph2m', 'wind_speed', 'wind_angle', 'mtpr', 'msr', 'msl', 'msdwswrf', 'msdwlwrf']
+
+    for variable in var_names:
         print('Processing '+variable)
         if variable == 'mtpr':
             varname = 'avg_tprate'
@@ -539,12 +544,31 @@ def era5_clim_for_ukesm (era5_dir='/gws/ssde/j25b/terrafirma/kaight/NEMO_AIS/UKE
         era5_time_mean_forcing(variable, year_start=1979, year_end=year_end, freq='3-hourly', era5_folder_in=era5_dir, era5_folder=None, processed=False, varname=varname)
 
 
-# Calculate monthly climatology for UKESM historical atmospheric forcing. So far just one ensemble member (cy691).
+# Calculate monthly climatology for 3-hourly UKESM historical atmospheric forcing. So far just one ensemble member (cy691).
 # Options for variable = tair, qair, wind_speed, wind_angle, precip, snow, pair, swrad, lwrad
 def ukesm_hist_forcing_monthly_clim (variable, in_dir='/gws/ssde/j25b/terrafirma/kaight/NEMO_AIS/UKESM_forcing/nemo_forcing_files/cy691/', out_dir=None, start_year=1979, end_year=2014):
 
     if out_dir is None:
         out_dir = in_dir + '/climatology/'
+
+    if variable in ['wind_speed', 'wind_angle']:
+        data_u = xr.open_mfdataset(f'{in_dir}uwind_*')['uwind']
+        data_v = xr.open_mfdataset(f'{in_dir}vwind_*')['vwind']
+        time_years = data_u['time'].dt.year
+        time_sel = (time_years >= start_year)*(time_years <= end_year)*(time_year != 1996)
+        data_u = data_u.isel('time'=time_sel)
+        data_v = data_v.isel('time'=time_sel)
+        if variable == 'wind_speed':
+            data_ds = np.hypot(data_u, data_v).rename(variable).to_dataset()
+        elif variable == 'wind_angle':
+            data_ds = np.arctan2(data_v, data_u).rename(variable).to_dataset()
+    else:
+        data = xr.open_mfdataset(f'{in_dir}{variable}_*.nc')[variable]
+        time_years = data['time'].dt.year
+        time_sel = (time_years >= start_year)*(time_years <= end_year)*(time_year != 1996)
+        data_ds = data.isel('time'=time_sel)
+    time_mean = data_ds.groupby('time.month').mean(dim='time')
+    time_mean.to_netcdf(f'{out_dir}{variable}_{start_year}-{end_year}_mean_monthly.nc')           
 
 
 # Function calculates the monthly time-mean over specified year range for mean of all CESM2 ensemble members in the specified experiment (for bias correction)
